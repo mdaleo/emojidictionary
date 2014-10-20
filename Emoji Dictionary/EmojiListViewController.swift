824ba533a5d19100788235a4c14678c2e964e5a1
Michael Daleo <mike@novocentpartners.com>
2014-10-20 15:40:16 -0400

    Initial commit

---
+++ Emoji Dictionary/EmojiListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiList.swift
+//  Emoji Dictionary
+//
+//  Created by MacBook on 10/20/14.
+//  Copyright (c) 2014 MacBook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EmojiListViewController : UIViewController {
+    
+}
