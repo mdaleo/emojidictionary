3ab728825df431fc2b91370635693c55ccf60f16
Michael Daleo <mike@novocentpartners.com>
2014-10-23 15:29:14 -0400

    Up to lecture 16

--- Emoji Dictionary/EmojiListViewController.swift
+++ Emoji Dictionary/EmojiListViewController.swift
@@ -9,6 +9,21 @@
 import Foundation
 import UIKit
 
-class EmojiListViewController : UIViewController {
+class EmojiListViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        self.tableView.dataSource = self;
+        self.tableView.delegate = self;
+    }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.backgroundColor = UIColor.brownColor()
+        cell.textLabel.text = "😍"
+        return cell
+    }
 }
